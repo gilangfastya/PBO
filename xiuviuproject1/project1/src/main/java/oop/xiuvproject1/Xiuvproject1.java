6d03dca1b70f7f0d6702dc6282bd3d0bd7a609ed
gilangfastya <tlangnation@gmail.com>
2026-09-01 23:42:49 +0700

    Add files via upload

---
+++ xiuviuproject1/project1/src/main/java/oop/xiuvproject1/Xiuvproject1.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package oop.xiuvproject1;
+
+/**
+ *
+ * @author acer
+ */
+public class Xiuvproject1 {
+
+    public static void main(String[] args) {
+        System.out.println("Nama  : GIlang Fastya");
+        System.out.println("NPM   : 0043");
+        System.out.println("Alamat: New York");
+        System.out.println("No HP : 082565255458");
+    }
+}
